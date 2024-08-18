3fdb34251e0be40e3133ce974ea659838fc67215
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-19 00:51:26 +0300

    complete labs

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+
+
+const  reverse=(string)=> {
+    return string.split("").reverse().join("");
+  }
+  
+  export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
